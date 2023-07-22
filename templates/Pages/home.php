5f3b12eb4abe9e5a0b9f4d11c7b3f5284006a787
Gabriel <gabriel.magalhaes.dev@gmail.com>
2023-07-22 13:24:33 -0300

    New Base

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -13,6 +14,7 @@
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
+
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
@@ -50,6 +52,7 @@ endif;
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -67,6 +70,7 @@ endif;
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
+
 <body>
     <header>
         <div class="container text-center">
@@ -102,54 +106,54 @@ endif;
                     <div class="column">
                         <h4>Environment</h4>
                         <ul>
-                        <?php if (version_compare(PHP_VERSION, '7.4.0', '>=')) : ?>
-                            <li class="bullet success">Your version of PHP is 7.4.0 or higher (detected <?= PHP_VERSION ?>).</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP is too low. You need PHP 7.4.0 or higher to use CakePHP (detected <?= PHP_VERSION ?>).</li>
-                        <?php endif; ?>
+                            <?php if (version_compare(PHP_VERSION, '7.4.0', '>=')) : ?>
+                                <li class="bullet success">Your version of PHP is 7.4.0 or higher (detected <?= PHP_VERSION ?>).</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your version of PHP is too low. You need PHP 7.4.0 or higher to use CakePHP (detected <?= PHP_VERSION ?>).</li>
+                            <?php endif; ?>
 
-                        <?php if (extension_loaded('mbstring')) : ?>
-                            <li class="bullet success">Your version of PHP has the mbstring extension loaded.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP does NOT have the mbstring extension loaded.</li>
-                        <?php endif; ?>
+                            <?php if (extension_loaded('mbstring')) : ?>
+                                <li class="bullet success">Your version of PHP has the mbstring extension loaded.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your version of PHP does NOT have the mbstring extension loaded.</li>
+                            <?php endif; ?>
 
-                        <?php if (extension_loaded('openssl')) : ?>
-                            <li class="bullet success">Your version of PHP has the openssl extension loaded.</li>
-                        <?php elseif (extension_loaded('mcrypt')) : ?>
-                            <li class="bullet success">Your version of PHP has the mcrypt extension loaded.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP does NOT have the openssl or mcrypt extension loaded.</li>
-                        <?php endif; ?>
+                            <?php if (extension_loaded('openssl')) : ?>
+                                <li class="bullet success">Your version of PHP has the openssl extension loaded.</li>
+                            <?php elseif (extension_loaded('mcrypt')) : ?>
+                                <li class="bullet success">Your version of PHP has the mcrypt extension loaded.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your version of PHP does NOT have the openssl or mcrypt extension loaded.</li>
+                            <?php endif; ?>
 
-                        <?php if (extension_loaded('intl')) : ?>
-                            <li class="bullet success">Your version of PHP has the intl extension loaded.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP does NOT have the intl extension loaded.</li>
-                        <?php endif; ?>
+                            <?php if (extension_loaded('intl')) : ?>
+                                <li class="bullet success">Your version of PHP has the intl extension loaded.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your version of PHP does NOT have the intl extension loaded.</li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                     <div class="column">
                         <h4>Filesystem</h4>
                         <ul>
-                        <?php if (is_writable(TMP)) : ?>
-                            <li class="bullet success">Your tmp directory is writable.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your tmp directory is NOT writable.</li>
-                        <?php endif; ?>
+                            <?php if (is_writable(TMP)) : ?>
+                                <li class="bullet success">Your tmp directory is writable.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your tmp directory is NOT writable.</li>
+                            <?php endif; ?>
 
-                        <?php if (is_writable(LOGS)) : ?>
-                            <li class="bullet success">Your logs directory is writable.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your logs directory is NOT writable.</li>
-                        <?php endif; ?>
+                            <?php if (is_writable(LOGS)) : ?>
+                                <li class="bullet success">Your logs directory is writable.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your logs directory is NOT writable.</li>
+                            <?php endif; ?>
 
-                        <?php $settings = Cache::getConfig('_cake_core_'); ?>
-                        <?php if (!empty($settings)) : ?>
-                            <li class="bullet success">The <em><?= h($settings['className']) ?></em> is being used for core caching. To change the config edit config/app.php</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your cache is NOT working. Please check the settings in config/app.php</li>
-                        <?php endif; ?>
+                            <?php $settings = Cache::getConfig('_cake_core_'); ?>
+                            <?php if (!empty($settings)) : ?>
+                                <li class="bullet success">The <em><?= h($settings['className']) ?></em> is being used for core caching. To change the config edit config/app.php</li>
+                            <?php else : ?>
+                                <li class="bullet problem">Your cache is NOT working. Please check the settings in config/app.php</li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
@@ -161,29 +165,29 @@ endif;
                         $result = $checkConnection('default');
                         ?>
                         <ul>
-                        <?php if ($result['connected']) : ?>
-                            <li class="bullet success">CakePHP is able to connect to the database.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">CakePHP is NOT able to connect to the database.<br /><?= h($result['error']) ?></li>
-                        <?php endif; ?>
+                            <?php if ($result['connected']) : ?>
+                                <li class="bullet success">CakePHP is able to connect to the database.</li>
+                            <?php else : ?>
+                                <li class="bullet problem">CakePHP is NOT able to connect to the database.<br /><?= h($result['error']) ?></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                     <div class="column">
                         <h4>DebugKit</h4>
                         <ul>
-                        <?php if (Plugin::isLoaded('DebugKit')) : ?>
-                            <li class="bullet success">DebugKit is loaded.</li>
-                            <?php
-                            $result = $checkConnection('debug_kit');
-                            ?>
-                            <?php if ($result['connected']) : ?>
-                                <li class="bullet success">DebugKit can connect to the database.</li>
+                            <?php if (Plugin::isLoaded('DebugKit')) : ?>
+                                <li class="bullet success">DebugKit is loaded.</li>
+                                <?php
+                                $result = $checkConnection('debug_kit');
+                                ?>
+                                <?php if ($result['connected']) : ?>
+                                    <li class="bullet success">DebugKit can connect to the database.</li>
+                                <?php else : ?>
+                                    <li class="bullet problem">DebugKit is <strong>not</strong> able to connect to the database.<br /><?= $result['error'] ?></li>
+                                <?php endif; ?>
                             <?php else : ?>
-                                <li class="bullet problem">DebugKit is <strong>not</strong> able to connect to the database.<br /><?= $result['error'] ?></li>
+                                <li class="bullet problem">DebugKit is <strong>not</strong> loaded.</li>
                             <?php endif; ?>
-                        <?php else : ?>
-                            <li class="bullet problem">DebugKit is <strong>not</strong> loaded.</li>
-                        <?php endif; ?>
                         </ul>
                     </div>
                 </div>
@@ -230,4 +234,5 @@ endif;
         </div>
     </main>
 </body>
+
 </html>
