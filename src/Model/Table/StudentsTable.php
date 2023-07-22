f6e9ec18dd0ccd4590c8777e50475583543ec297
Gabriel <gabriel.magalhaes.dev@gmail.com>
2023-07-22 12:49:10 -0300

    Base

---
+++ src/Model/Table/StudentsTable.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Students Model
+ *
+ * @property \App\Model\Table\StudentAttendanceTable&\Cake\ORM\Association\HasMany $StudentAttendance
+ *
+ * @method \App\Model\Entity\Student newEmptyEntity()
+ * @method \App\Model\Entity\Student newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Student[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Student get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Student findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Student patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Student[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Student|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Student saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Student[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Student[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Student[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Student[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ */
+class StudentsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('students');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->hasMany('StudentAttendance', [
+            'foreignKey' => 'student_id',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 100)
+            ->requirePresence('name', 'create')
+            ->notEmptyString('name');
+
+        $validator
+            ->dateTime('created_at')
+            ->notEmptyDateTime('created_at');
+
+        $validator
+            ->dateTime('updated_at')
+            ->notEmptyDateTime('updated_at');
+
+        return $validator;
+    }
+}
